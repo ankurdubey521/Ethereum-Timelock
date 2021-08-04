08970ae71a376e3e3829c2ae2b482914e6d31da4
Ankur Dubey <ankurdubey521@protonmail.com>
2021-08-04 17:27:01 +0530

    Initialize Contracts

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,8 @@
+import "@nomiclabs/hardhat-waffle";
+import type { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+};
+
+export default config;
