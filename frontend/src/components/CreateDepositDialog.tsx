b9269617b424416c3188c16f0498be1ff84e94e2
Ankur Dubey <ankurdubey521@protonmail.com>
2021-08-07 14:17:19 +0530

    Implement flow for ETH deposit

--- frontend/src/components/CreateDepositDialog.tsx
+++ frontend/src/components/CreateDepositDialog.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import MuiAlert, { AlertProps } from "@material-ui/lab/Alert";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 import { useWeb3React } from "@web3-react/core";
 import { Decimal } from "decimal.js";
 import { ethers } from "ethers";
@@ -67,6 +72,27 @@ export default function CreateDepositDialog(): JSX.Element {
     setOpen(false);
   };
 
+  const createEthDeposit = async () => {
+    try {
+      const actualTokenAmount = ethers.BigNumber.from(
+        new Decimal(amount).mul(new Decimal(10).pow(18)).toFixed()
+      );
+      const timeVaultUtil = new TimeVaultUtil(signer);
+
+      setStatusMessage("Creating Deposit...");
+      await timeVaultUtil.createEthTimeLockDeposit(
+        recipientAddress as string,
+        Math.floor((unlockDate?.getTime() as number) / 1000),
+        actualTokenAmount
+      );
+      setStatusMessage("");
+      handleClose();
+      window.location.reload(false);
+    } catch (e) {
+      console.error("Error Creating ERC20 Deposit: ", e);
+    }
+  };
+
   const createErc20Deposit = async () => {
     try {
       const erc20token = new ERC20Util(signer, tokenAddress as string);
@@ -122,15 +148,52 @@ export default function CreateDepositDialog(): JSX.Element {
             fullWidth
             onChange={(event) => setRecipientAddress(event.target.value)}
           />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Token"
-            type="text"
-            fullWidth
-            onChange={(event) => setTokenAddress(event.target.value)}
-          />
+          <FormControl>
+            <RadioGroup
+              value={
+                {
+                  [TimeLockDepositType.ETH]: "ETH",
+                  [TimeLockDepositType.ERC20]: "ERC20",
+                }[depositType]
+              }
+              onChange={(event) =>
+                setDepositType(
+                  {
+                    ETH: TimeLockDepositType.ETH,
+                    ERC20: TimeLockDepositType.ERC20,
+                  }[event.target.value] as TimeLockDepositType
+                )
+              }
+            >
+              <Grid container>
+                <Grid item>
+                  <FormControlLabel
+                    value="ETH"
+                    control={<Radio />}
+                    label="ETH"
+                  />
+                </Grid>
+                <Grid item>
+                  <FormControlLabel
+                    value="ERC20"
+                    control={<Radio />}
+                    label="ERC20"
+                  />
+                </Grid>
+              </Grid>
+            </RadioGroup>
+          </FormControl>
+          {depositType === TimeLockDepositType.ERC20 && (
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Token"
+              type="text"
+              fullWidth
+              onChange={(event) => setTokenAddress(event.target.value)}
+            />
+          )}
           <TextField
             autoFocus
             margin="dense"
@@ -155,7 +218,15 @@ export default function CreateDepositDialog(): JSX.Element {
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={createErc20Deposit} color="primary">
+          <Button
+            onClick={async () => {
+              await {
+                [TimeLockDepositType.ETH]: createEthDeposit,
+                [TimeLockDepositType.ERC20]: createErc20Deposit,
+              }[depositType]();
+            }}
+            color="primary"
+          >
             Create
           </Button>
         </DialogActions>
